5fd65c07bb462d4c98a7ac5ee84d6b1ece2b4163
H0MELANDER-EXE <109294582+H0MELANDER-EXE@users.noreply.github.com>
2022-07-18 17:30:54 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,8 +10,8 @@ URL | filename | username | password"""
     DOWNLOAD_START = "📥Downloading..."
     UPLOAD_START = "📤Uploading..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @VENGEANCE_URL_UPLOADER_BOT\n\n<b>Join : @MIDDLEEASTDESTROYER</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@VENGEANCE_URL_UPLOADER_BOT"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @BLOODSHOT_URL_UPLOADER_BOT\n\n<b>Join : @BLOODSHOTBOTS</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@BLOODSHOT_URL_UPLOADER_BOT"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -26,7 +26,7 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, contact @Teamx69"""
+If bot didn't respond, contact @BL00DSH0T"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
